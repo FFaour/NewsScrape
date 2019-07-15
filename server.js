f7b1a398765c5f00b6f56c77222b41012fc244b2
FRED F <FrederickFaour@github.com>
2019-07-14 23:04:52 -0400

    Adding work in progress

--- server.js
+++ server.js
@@ -1,27 +1,42 @@
-const express = require("express");
-const axios = require("axios");
-const mongoose = require("mongoose");
-const router = require("./router");
-const app = express();
-const PORT = process.env.PORT || 3001;
+const express = require('express');
+const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
+const PORT = process.env.PORT || 3000;
 
-// Define middleware here
+// instantiate express
+var app = express();
+
+// setup the express router
+var router = express.Router();
+
+// Require the routes file and pass the Router object
+require("/config/routes")(router);
+
+// point to the public dir for static files served
+app.use(express.static(__dirname + 'public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Connect Handlebars to our Express app
+app.engine("handlebars", expressHandlebars({
+  defualtlayout: "main"
+}));
+app.set("view engine", "handlebars");
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-} else {
-  app.use(express.static("public"));
-}
-
-// Add routes, both API and view
+// use the router as middleware
 app.use(router);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/scrapedNews");
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
+
+// connect to the database using mongoose and log any connection error
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, function(error) {
+  if (error) {
+    console.log("Mongoose connection error!");
+  } else {
+    console.log("Mongoose connected to " + MONGODB_URI + "!");
+  }
+});
 
 // Start the API server
 app.listen(PORT, function() {
